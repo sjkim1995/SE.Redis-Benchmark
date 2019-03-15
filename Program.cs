6d6a9a399e9bc673a2d2caf1547b87717dd64aa2
SJ Kim <seki@microsoft.com>
2019-03-15 14:28:02 -0700

    Working implementation SE.Redis Benchmark

--- Program.cs
+++ Program.cs
@@ -6,60 +6,51 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Redis_Benchmark
 {
   
     class Program
     {
-        // Timing
+        // Globals 
         static DateTime startTime;
+        static double trialTimeInSecs;
+        static int numTrials; 
 
-        // Latency
-        static long _totalLatencyInTicks;
         static ConcurrentBag<double> latencyBag;
-
-        // Requests
         static long _totalRequests;
 
-        // Request details
         static string host;
+        static string key;
         static byte[] value;
-        static long concurrentOps;
-        static readonly string key = "test";
         const int keySizeBytes = 1024;
-
-        static void SetGlobals()
-        {
-            _totalLatencyInTicks = _totalRequests = 0;
-            startTime = DateTime.Now;
-            value = new byte[keySizeBytes];
-            (new Random()).NextBytes(value);
-        }
+        static long parallelOps;
 
         static void PrintTestParams()
         {
             Console.WriteLine("Host:\t\t\t{0}", host);
-            Console.WriteLine("PendingRequests:\t{0}", concurrentOps);
+            Console.WriteLine("PendingRequests:\t{0}", parallelOps);
             Console.WriteLine("Cache Item Size:\t{0} bytes", keySizeBytes);
-            Console.WriteLine();
+            Console.WriteLine("Time per trial:\t\t{0}s", trialTimeInSecs);
+            Console.WriteLine("# Trials:\t\t{0}\n", numTrials);
         }
 
-        static void Main(string[] args)
+        static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+
+        async static Task MainAsync(string[] args)
         {
             if (!ExecutionContext.IsFlowSuppressed())
             {
                 ExecutionContext.SuppressFlow();
             }
 
-            SetGlobals();
-
             // Command-line args
             host = args[0];
             string password = args[1];
-            concurrentOps = Int64.Parse(args[2]);
-            double testPeriodInMins = Double.Parse(args[3]);
-            int numTrials = Int32.Parse(args[4]);
+            parallelOps = Int64.Parse(args[2]);
+            trialTimeInSecs = Double.Parse(args[3]);
+            numTrials = Int32.Parse(args[4]);
             string fileName = args[5];
 
             PrintTestParams();
@@ -69,54 +60,68 @@ namespace Redis_Benchmark
             {
                 CommandMap = CommandMap.Create(new HashSet<string>(new string[] { "SUBSCRIBE" }), false),
                 Ssl = false,
-                ResponseTimeout = 1,
                 Password = password,
                 AbortOnConnectFail = true
             };
             config.EndPoints.Add(host);
        
+            // Initialize CSV to store results and write header values
+            StringBuilder csv = new StringBuilder();
+            csv.AppendLine("Host, Start Time, Trial Time, # Trials");
+            csv.AppendLine($"{host}, {DateTime.Now}, {trialTimeInSecs}, {numTrials}");
+            csv.AppendLine();
+            csv.AppendLine("Median Latency (ms), Avg Latency (ms), Throughput (bytes/sec)"); // Column headers
+
             // Get redis instance and flush database
             ConnectionMultiplexer cm = ConnectionMultiplexer.Connect(config);
             IDatabase redis = cm.GetDatabase();
+
+            // Set the test key 
+            key = "test";
+            value = new byte[keySizeBytes];
+            (new Random()).NextBytes(value);
             redis.StringSet(key, value);
 
-            // Initialize CSV to store results and write header values
-            StringBuilder csv = new StringBuilder();
-            csv.AppendLine($"Host: {host}");
-            csv.AppendLine($"Start time: {startTime}");
-            csv.AppendLine();
-            // columns
-            csv.AppendLine("Median Latency (ms), Avg Latency (ms), Throughput (bytes/sec)");
-
-            CPUAndMemoryLogger cpuMemLogger = new CPUAndMemoryLogger();
+            CPUAndMemoryLogger cpuMemLogger = new CPUAndMemoryLogger(); // Start logging CPU and Memory on a separate thread
 
             for (int i = 0; i < numTrials; i++)
             {
+                Console.WriteLine($"*RUNNING TRIAL {i + 1}");
+
+                // reset the latency buffer, request count, and start time for the new trial
                 latencyBag = new ConcurrentBag<double>();
+                Interlocked.Exchange(ref _totalRequests, 0);
+                startTime = DateTime.Now;
 
-                Console.WriteLine($"Running trial {i + 1}");
-                // Start the request process
-                DoRequests(redis);
+                // Start making concurrent requests
+                var cancel = new CancellationTokenSource();
+                var requestTasks = DoRequests(redis, cancel.Token);
 
-                while ((DateTime.Now - startTime).TotalMinutes < testPeriodInMins) { /* spin */ }
+                await Task.Delay(TimeSpan.FromSeconds(trialTimeInSecs));
+
+                // to cancel
+                cancel.Cancel();
+
+                // finish current calls
+                await requestTasks;
 
                 double[] sortedLatency = latencyBag.ToArray();
                 Array.Sort(sortedLatency);
 
                 double avgLatency = 0;
-                int median = sortedLatency.Length / 2;
+                double medianLatency = sortedLatency[sortedLatency.Length / 2];
                 foreach (var lat in sortedLatency)
                 {
                     avgLatency += lat;
                 }
                 avgLatency /= _totalRequests;
-                double throughputMB = Math.Round((_totalRequests * keySizeBytes) / (DateTime.Now - startTime).TotalSeconds, 2);
+                double throughputMB = Math.Round((_totalRequests * keySizeBytes) / (DateTime.Now - startTime).TotalSeconds, 2) / (1024 * 1024);
 
-                Console.WriteLine("Median Latency: {0} ms", sortedLatency[median]);
+                Console.WriteLine("Median Latency: {0} ms", medianLatency);
                 Console.WriteLine("Average Latency: {0} ms", avgLatency);
-                Console.WriteLine("Throughput: {0} bytes/s\n", throughputMB);
+                Console.WriteLine("Throughput: {0} MB/s\n", throughputMB);
 
-                csv.AppendLine($"{sortedLatency[median]}, {avgLatency}, {throughputMB}");
+                csv.AppendLine($"{medianLatency}, {avgLatency}, {throughputMB}");
             }
 
             // line to seperate the CPU and Memory stats
@@ -128,30 +133,32 @@ namespace Redis_Benchmark
 
             return;
         }
-        
 
-        static int DoRequests(IDatabase redis)
+
+        async static Task DoRequests(IDatabase redis, CancellationToken t)
         {
-            for (int i = 0; i < concurrentOps; i++)
+            var calls = new List<Task>();
+            for (int i = 0; i < parallelOps; i++)
             {
-                DoCalls(redis);
+               calls.Add(DoCalls(redis, t));
             }
-            return 0;
+            await Task.WhenAll(calls);
         }
 
-        static void DoCalls(IDatabase redis)
+        async static Task DoCalls(IDatabase redis, CancellationToken t)
         {
-            var sw = Stopwatch.StartNew();
-            long latency = 0;
+            var sw = new Stopwatch();
 
-            redis.StringGetAsync(key).ContinueWith((v) =>
+            while (!t.IsCancellationRequested)
             {
-                latency += sw.Elapsed.Ticks;
+                sw.Restart();
+                await redis.StringGetAsync(key);
+                var elapsedMs = sw.ElapsedMilliseconds;
                 Interlocked.Increment(ref _totalRequests);
-                double latencyInMs = Math.Round((double)(latency / TimeSpan.TicksPerMillisecond), 2);
-                latencyBag.Add(latencyInMs);
-                DoCalls(redis);
-            });
+                latencyBag.Add(elapsedMs);
+            }
+
+            return;
         }
     }
 }
